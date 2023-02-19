d0e0be90b43997434f91e6cfd975852c34cb292f
miguelamaral254 <118134009+miguelamaral254@users.noreply.github.com>
2023-02-19 02:15:46 -0300

    branch#20

--- costs/src/Components/Pages/Home.js
+++ costs/src/Components/Pages/Home.js
@@ -1,6 +1,18 @@
+import styles from './Home.module.css'
+import savings from '../../Img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+import { Link } from 'react-router-dom'
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Bem vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Create a new project"/>    
+            <img src={savings} alt="costs"/>
+
+        </section>
+        
     )
 }
 export default Home
